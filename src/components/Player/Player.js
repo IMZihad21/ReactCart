55ffe41996636d60884d7f726ae13ae84ef64e28
ZèD <imzihad@gmail.com>
2021-09-26 15:55:31 +0600

    Fix a small typo
    Signed-off-by: ZèD <imzihad@gmail.com>

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -11,7 +11,7 @@ const Player = (props) => {
             <p><span className="font-semibold">Region:</span> {region}</p>
             <p><span className="font-semibold">Birthday:</span> {birthday}</p>
             <p><span className="font-semibold">Price:</span> ${price}</p>
-            <button onClick={()=> props.handleAddToCart(props.player)} class="bg-blue-500 hover:bg-blue-700 text-white font-bold m-2 py-2 px-6 border border-blue-700 rounded-lg">
+            <button onClick={()=> props.handleAddToCart(props.player)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold m-2 py-2 px-6 border border-blue-700 rounded-lg">
                 Add to Cart
             </button>
         </div>
