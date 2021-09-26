8415a0667bdcb664f45e0056209e6be65d47607d
ZèD <imzihad@gmail.com>
2021-09-26 11:59:03 +0600

    Fix Json and add Cart component

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -4,7 +4,8 @@ const Player = (props) => {
     const {name, title, image, rarity, element, weapontype, region, birthday} = props.player;
     return (
         <div>
-            
+            <img src={image} alt="" />
+            <h1>Name: {name}</h1>
         </div>
     );
 };
--- src/components/Players/Players.js
+++ src/components/Players/Players.js
@@ -1,21 +1,30 @@
 import React, { useEffect, useState } from 'react';
+import Cart from '../Cart/Cart';
 import Player from '../Player/Player';
 
 const Players = () => {
-    const [players, setPlayers] = useState([]);
-    useEffect(()=>{
+    const [ players, setPlayers ] = useState([]);
+    
+    useEffect(() => {
         fetch('./players.JSON')
             .then(res => res.json())
             .then(data => setPlayers(data));
-    },[])
+    }, [])
+
+
     return (
         <div>
-            {
-                players.map(player => <Player
+            <div>
+                {
+                    players.map(player => <Player
                         player={player}
                         key={player.id}
                     />)
-            }
+                }
+            </div>
+            <div>
+                <Cart />
+            </div>
         </div>
     );
 };
