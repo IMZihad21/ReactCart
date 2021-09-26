1ae2ab024701ce45d0d589ce0119285f181640c2
ZèD <imzihad@gmail.com>
2021-09-26 18:16:25 +0600

    Small changes in Header
    Signed-off-by: ZèD <imzihad@gmail.com>

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
         <div className="shadow-lg bg-gray-200 px-2 py-4 rounded-lg">
             <h1 className="text-4xl font-bold pb-4 ">Genshin Characters</h1>
             <p className="text-xl pb-2"><strong>Make A Dream Team</strong> With the amazing characters below, lets build a strong party for Genshin Impact</p>
-            <h1 className="text-2xl">Close to <span className="font-bold text-blue-500">${174000000}</span> in revenue from actual game</h1>
+            <h1 className="text-2xl">Total budget around <span className="font-bold text-blue-500">${174}</span> Million</h1>
         </div>
     );
 };
